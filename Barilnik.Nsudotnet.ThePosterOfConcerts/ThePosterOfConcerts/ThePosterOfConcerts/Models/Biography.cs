517f6a0214d2273a075f34a8b3f7c6c9d9c5ad41
barilnikks <barilnikks@gmail.com>
2016-05-24 12:37:04 +0600

    no message

---
+++ Barilnik.Nsudotnet.ThePosterOfConcerts/ThePosterOfConcerts/ThePosterOfConcerts/Models/Biography.cs
@@ -0,0 +1,14 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePosterOfConcerts.Models
+{
+    class Biography
+    {
+        public int PerformerID { get; set; }
+        public String PerformerBiography { get; set; }
+    }
+}
---
+++ Barilnik.Nsudotnet.ThePosterOfConcerts/ThePosterOfConcerts/ThePosterOfConcerts/Models/Performer.cs
@@ -0,0 +1,17 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace ThePosterOfConcerts.Models
+{
+    class Performer
+    {
+        public int PerformerID { get; set; }
+        public String Name { get; set; }
+        public Biography biography { get; set; }
+        public String Country { get; set; }
+        public String Genre { get; set; }
+    }
+}
